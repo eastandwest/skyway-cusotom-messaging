fe2e0de1670a49241cb2b9b6716b4cbb4f62faf5
kensaku komatsu <kensaku.komatsu@gmail.com>
2016-03-08 18:21:33 -0800

    for issue #10, validation and test code is appended

--- test/unit/peer_custom_mesg.test.js
+++ test/unit/peer_custom_mesg.test.js
@@ -74,8 +74,87 @@ describe('#_signalling_mesg()', function(){
     expect(custom._is_signalling_mesg("PING_")).to.be.false;
     expect(custom._is_signalling_mesg("_PING")).to.be.false;
   });
-
 });
+
+describe('#_create_payload()', function(){
+  var custom = new PeerCustomMesg(peer, "UNITTEST");
+
+  it('should return valid object, when parameter is valid', function(){
+    expect(custom._create_payload("hello", false)).to.deep.equal({
+      "data": "hello",
+      "is_rpc": false
+    });
+
+    var res = custom._create_payload("hello", true, true)
+    expect(res).to.have.deep.property("data", "hello")
+    expect(res).to.have.deep.property("is_rpc", true)
+    expect(res).to.have.deep.property("from", "sender")
+    expect(res.transaction_id).to.match(/^[0-9a-z]{32}$/)
+
+    var res = custom._create_payload("hello", true, false, "01234567890123456789012345678901")
+    expect(res).to.have.deep.property("data", "hello")
+    expect(res).to.have.deep.property("is_rpc", true)
+    expect(res).to.have.deep.property("from", "receiver")
+    expect(res.transaction_id).to.equal("01234567890123456789012345678901")
+  });
+
+  it('should throw error, when data is not valid', function(){
+    expect(function(){custom._create_payload(null, false)}).to.throw("data should not be falthy");
+
+    expect(function(){custom._create_payload("hello", 0)}).to.throw("is_rpc should be boolean");
+
+    expect(function(){custom._create_payload("hello", true, 0)}).to.throw("is_sender should be boolean");
+
+    expect(function(){custom._create_payload("hello", true, false, null)}).to.throw("transaction_id does not match with the format");
+    expect(function(){custom._create_payload("hello", true, false, "0123456789012345678901234567890")}).to.throw("transaction_id does not match with the format");
+    expect(function(){custom._create_payload("hello", true, false, "012345678901234567890123456789012")}).to.throw("transaction_id does not match with the format");
+  });
+});
+
+describe("#_make_transaction_id()", function(){
+  var custom = new PeerCustomMesg(peer, "UNITTEST");
+
+  it("should make valid format of transaction id", function(){
+    expect(custom._make_transaction_id()).to.match(/^[0-9a-z]{32}$/);
+  });
+});
+
+describe("#_is_boolean()", function(){
+  var custom = new PeerCustomMesg(peer, "UNITTEST");
+
+  it("should be true if val is boolan", function(){
+    expect(custom._is_boolean(true)).to.be.true;
+    expect(custom._is_boolean(false)).to.be.true;
+  });
+
+  it("should be false if val is not boolan", function(){
+    expect(custom._is_boolean(null)).to.be.false;
+    expect(custom._is_boolean(1)).to.be.false;
+    expect(custom._is_boolean("hoge")).to.be.false;
+    expect(custom._is_boolean({})).to.be.false;
+  });
+});
+
+describe("#_is_transaction_id()", function(){
+  var custom = new PeerCustomMesg(peer, "UNITTEST");
+
+  it("should be true if transaction_id is 32 bytes long and contains number and downcase alphabet only", function(){
+    expect(custom._is_transaction_id("0123456789abcdefghij0123456789kl")).to.be.true;
+    expect(custom._is_transaction_id("0123456789mnopqrstuv01234567wxyz")).to.be.true;
+  });
+  it("should be false if transaction_id is not string", function(){
+    expect(custom._is_transaction_id(null)).to.be.false;
+    expect(custom._is_transaction_id(123)).to.be.false;
+  });
+  it("should be false if transaction_id is string but not 32 bytes long or contains uppercase alphabet or non-ascii type string", function(){
+    expect(custom._is_transaction_id("1234567890123456789012345678901")).to.be.false;
+    expect(custom._is_transaction_id("123456789012345678901234567890123")).to.be.false;
+    expect(custom._is_transaction_id("123456789A1234567890123456789012")).to.be.false;
+    expect(custom._is_transaction_id("123456789あ1234567890123456789012")).to.be.false;
+  });
+});
+
+
 describe('#_validate_send()', function(){
   var custom = new PeerCustomMesg(peer, "UNITTEST");
 
